54358e2d26a2adab0c94d1ec2492782f93cad8f3
Thomas Donahue <donahue777@yahoo.com>
2024-02-29 13:22:32 -0500

    All done

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace Classes
+﻿using System.Xml.Schema;
+
+namespace Classes
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Subaru";
+            myCar.Model = "wrx";
+            myCar.Year = 2020;
+
+            var ferrari = new Car()
+            {
+                Make = "ferrari",
+                Model = "SF90",
+                Year = 2019
+            };
+
+            var ford = new Car("ford", "gt500", 1967);
+
+            var carList = new List<Car>() { myCar, ferrari, ford };
+
+            foreach(var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
         }
     }
 }
